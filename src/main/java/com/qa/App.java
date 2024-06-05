4e3d6efe18a985af1caa6d3c3385975b9349af29
Tom <curlywehrle5991@hotmail.com>
2024-06-05 11:22:42 +0100

    added hello carmen

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -14,6 +14,7 @@ public class App
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
         System.out.println(sayHelloToSomeone("Tom"));
+        System.out.println(sayHelloToSomeone("Carmen"));
         System.out.println(sayGoodbye());
     }
 
